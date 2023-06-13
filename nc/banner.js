fb6e54e83c63beaf9f3898cc3ce6d1088c20e387
Francis Akoji <akfrendo@gmail.com>
2023-06-13 08:45:21 +0100

    Update banner.js

--- nc/banner.js
+++ nc/banner.js
@@ -1,7 +1,7 @@
-const closeBtn = document.getElementById("close");
-const container = document.getElementById("container");
+const closeBtn = document.getElementById("banner-close");
+const container = document.getElementById("banner-container");
 
-const banner = document.getElementsByClassName("header");
+const banner = document.getElementsByClassName("banner-header");
 closeBtn.addEventListener("click", function () {
   container.style.display = "none";
 });
